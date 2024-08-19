0830eba40119162054f45d7772929102629560a7
poffomoe <128525238+poffomoe@users.noreply.github.com>
2024-08-19 19:36:35 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Conway's Game of Life
+
+*(I haven't programmed for too long. Thought it would be a good idea to get back into it by coding the Conway's Game of Life in Rust. Could have coded something else but I am not creative enough to think of any worthy project idea.)*
+
+## Purpose
+
+Purpose of this project was to code something in Rust already.
+
+I chose to code Game of Life for the first time in my life while using a game engine that doesn't have a GUI. I've done neither before, valuable experience.
+
+## Features
+
+This is just Game of Life, nothing super interesting, but you can:
+
+- **set window size** and **grid resolution** inside the `main.rs` file
+- **pause** the game by pressing `Space`
+- **speed the game up** by holding `LShift`
+- **draw** live cells with `LMB`
+- **unalive** live cells with `RMB` 😈
+- **unalive** ALL cells by pressing `C` 😈😈😈
+
+## todo (maybe)
+
+- [ ] Zooming and panning
+- [ ] Next step
+- [ ] Current generation, live cells
+
+## Credits
+
+https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life  
+https://github.com/PistonDevelopers/Piston-Tutorials/  
+https://github.com/PistonDevelopers/piston-examples  
+
---
+++ src/main.rs
@@ -0,0 +1,186 @@
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateEvent};
+use piston::window::WindowSettings;
+use piston::{Button, EventLoop, MouseCursorEvent, PressEvent, ReleaseEvent};
+
+use std::time::Duration;
+
+const WINDOW_SIZE: usize = 512;
+const GRID_RES: usize = 32;
+
+const RECT_SIZE: f64 = (WINDOW_SIZE / GRID_RES) as f64;
+
+pub struct App {
+    gl: GlGraphics,
+    paused: bool,
+    grid: [[bool; GRID_RES]; GRID_RES],
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+        self.gl.draw(args.viewport(), |c: Context, gl| {
+            clear(BLACK, gl);
+
+            for x in 0..GRID_RES {
+                for y in 0..GRID_RES {
+                    let rect_size_minus_border: f64 = RECT_SIZE - 1.0;
+
+                    let transform: [[f64; 3]; 2] = c.transform.trans(
+                        x as f64 * rect_size_minus_border,
+                        y as f64 * rect_size_minus_border,
+                    );
+
+                    rectangle(
+                        if self.grid[x][y] { WHITE } else { BLACK },
+                        rectangle::square(x as f64, y as f64, RECT_SIZE as f64),
+                        transform,
+                        gl,
+                    );
+                }
+            }
+        });
+    }
+
+    fn update(&mut self) {
+        let mut newgrid: [[bool; GRID_RES]; GRID_RES] = self.grid;
+
+        for x in 0..GRID_RES {
+            for y in 0..GRID_RES {
+                if self.grid[x][y] == false {
+                    if count_neighbours(self.grid, x, y) == 3 {
+                        newgrid[x][y] = true;
+                    }
+                } else {
+                    if count_neighbours(self.grid, x, y) < 2 {
+                        newgrid[x][y] = false;
+                    } else if count_neighbours(self.grid, x, y) > 3 {
+                        newgrid[x][y] = false;
+                    }
+                }
+            }
+        }
+
+        self.grid = newgrid;
+    }
+}
+
+fn main() {
+    let opengl: OpenGL = OpenGL::V4_5;
+
+    let mut update_interval: f64 = 0.5;
+
+    let mut draw: bool = false;
+    let mut erase: bool = false;
+
+    let mut elapsed_time: Duration = Duration::from_millis(0);
+
+    let mut window: Window =
+        WindowSettings::new("Game of Life", [WINDOW_SIZE as f64, WINDOW_SIZE as f64])
+            .graphics_api(opengl)
+            .exit_on_esc(true)
+            .resizable(false)
+            .build()
+            .unwrap();
+
+    let mut app: App = App {
+        gl: GlGraphics::new(opengl),
+        paused: false,
+        grid: [[false; GRID_RES]; GRID_RES],
+    };
+
+    let mut events: Events = Events::new(EventSettings::new().max_fps(60));
+    while let Some(e) = events.next(&mut window) {
+        if let Some(button) = e.press_args() {
+            match button {
+                Button::Keyboard(piston::Key::Space) => app.paused = !app.paused,
+                Button::Keyboard(piston::Key::C) => app.grid = [[false; GRID_RES]; GRID_RES],
+                Button::Mouse(piston::MouseButton::Left) => {
+                    if !erase {
+                        draw = true
+                    }
+                }
+                Button::Mouse(piston::MouseButton::Right) => {
+                    if !draw {
+                        erase = true
+                    }
+                }
+                Button::Keyboard(piston::Key::LShift) => update_interval = 0.1,
+                _ => (),
+            }
+        };
+
+        if let Some(button) = e.release_args() {
+            match button {
+                Button::Mouse(piston::MouseButton::Left) => draw = false,
+                Button::Mouse(piston::MouseButton::Right) => erase = false,
+                Button::Keyboard(piston::Key::LShift) => update_interval = 0.5,
+                _ => (),
+            }
+        }
+
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if draw || erase {
+            if let Some(pos) = e.mouse_cursor_args() {
+                let (x, y) = (pos[0], pos[1]);
+
+                if (x > 0.0) && (y > 0.0) && (x < WINDOW_SIZE as f64) && (y < WINDOW_SIZE as f64) {
+                    let squarex: i32 = x as i32 / RECT_SIZE as i32;
+                    let squarey: i32 = y as i32 / RECT_SIZE as i32;
+
+                    app.grid[squarex as usize][squarey as usize] = draw;
+                }
+            }
+        }
+
+        if !app.paused {
+            if let Some(args) = e.update_args() {
+                elapsed_time += Duration::from_secs_f64(args.dt);
+                if elapsed_time.as_secs_f64() >= update_interval {
+                    app.update();
+                    elapsed_time -= Duration::from_secs_f64(update_interval)
+                }
+            }
+        }
+    }
+}
+
+fn count_neighbours(grid: [[bool; GRID_RES]; GRID_RES], x: usize, y: usize) -> u8 {
+    let mut count: u8 = 0;
+
+    let directions: [(i8, i8); 8] = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    for (dir_x, dir_y) in directions {
+        let neighbour_x: i8 = x as i8 + dir_x;
+        let neighbour_y: i8 = y as i8 + dir_y;
+
+        if (neighbour_x >= 0)
+            && (neighbour_y >= 0)
+            && (neighbour_x < GRID_RES as i8)
+            && (neighbour_y < GRID_RES as i8)
+            && (grid[neighbour_x as usize][neighbour_y as usize])
+        {
+            count += 1;
+        }
+    }
+
+    count
+}
